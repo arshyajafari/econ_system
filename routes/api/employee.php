10e158b8954bc56df844e2247c906fb04a568fc7
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:09:17 +0330

    feat: expose employee API routes

--- routes/api/employee.php
+++ routes/api/employee.php
@@ -1,1 +1,16 @@
 <?php
+
+use App\Http\Controllers\Api\EmployeeController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('employees')->name('employees.')->group(function () {
+    Route::get('/', [EmployeeController::class, 'index'])->name('index');
+    Route::post('/', [EmployeeController::class, 'store'])->name('store');
+    Route::get('/{employee}', [EmployeeController::class, 'show'])->name('show');
+    Route::put('/{employee}', [EmployeeController::class, 'update'])->name('update');
+    Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('destroy');
+    Route::patch('/{employee}/status', [EmployeeController::class, 'changeStatus'])->name('change-status');
+    Route::patch('/{employee}/restore', [EmployeeController::class, 'restore'])
+        ->withTrashed()
+        ->name('restore');
+});
