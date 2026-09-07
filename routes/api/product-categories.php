f788103cdec2ddda960706f3a37582ab07517cc2
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:09:35 +0330

    feat: add product category API routes

---
+++ routes/api/product-categories.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\ProductCategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('product-categories')->name('product-categories.')->group(function () {
+    Route::get('/', [ProductCategoryController::class, 'index'])->name('index');
+    Route::get('/tree', [ProductCategoryController::class, 'tree'])->name('tree');
+    Route::post('/', [ProductCategoryController::class, 'store'])->name('store');
+    Route::get('/{productCategory}', [ProductCategoryController::class, 'show'])->name('show');
+    Route::put('/{productCategory}', [ProductCategoryController::class, 'update'])->name('update');
+    Route::delete('/{productCategory}', [ProductCategoryController::class, 'destroy'])->name('destroy');
+    Route::patch('/{productCategory}/activity', [ProductCategoryController::class, 'changeActivity'])
+        ->name('change-activity');
+});
