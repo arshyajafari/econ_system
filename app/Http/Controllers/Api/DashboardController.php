b9a7690d91e32ff061e71a8bec9a4fc457977cfc
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:23:43 +0330

    fix: authorize dashboard access

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -1,13 +1,18 @@
 <?php
 
-    namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api;
 
-    use App\Actions\Dashboard\GetDashboardAction;
-    use App\Http\Controllers\Controller;
-    use App\Http\Resources\DashboardResource;
+use App\Actions\Dashboard\GetDashboardAction;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DashboardResource;
+use Illuminate\Http\Request;
 
-    class DashboardController extends Controller {
-        public function index(GetDashboardAction $action): DashboardResource {
-            return new DashboardResource($action->execute());
-        }
+class DashboardController extends Controller
+{
+    public function index(Request $request, GetDashboardAction $action): DashboardResource
+    {
+        $this->authorize('viewDashboard', $request->user());
+
+        return new DashboardResource($action->execute());
     }
+}
