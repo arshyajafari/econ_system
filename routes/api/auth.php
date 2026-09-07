84c38402ddb756f8441b141fa20dc711d338f6b3
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:19:03 +0330

    fix: simplify auth API routes

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,18 +1,14 @@
 <?php
 
-    use App\Http\Controllers\Api\AuthController;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
 
-    Route::prefix('auth')->name('auth.')->group(function () {
-        Route::post('/login', [
-            AuthController::class,
-            'login',
-        ])->name('login');
-    });
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])
+        ->middleware('throttle:10,1')
+        ->name('login');
+});
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/auth/logout', [
-            AuthController::class,
-            'logout',
-        ])->name('auth.logout');
-    });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
