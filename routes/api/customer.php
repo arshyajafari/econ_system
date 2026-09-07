10d9476ea9354884ab206fe9c1f500d01f0524cc
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:08:57 +0330

    feat: expose customer API routes

--- routes/api/customer.php
+++ routes/api/customer.php
@@ -1,1 +1,16 @@
 <?php
+
+use App\Http\Controllers\Api\CustomerController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('customers')->name('customers.')->group(function () {
+    Route::get('/', [CustomerController::class, 'index'])->name('index');
+    Route::post('/', [CustomerController::class, 'store'])->name('store');
+    Route::get('/{customer}', [CustomerController::class, 'show'])->name('show');
+    Route::put('/{customer}', [CustomerController::class, 'update'])->name('update');
+    Route::delete('/{customer}', [CustomerController::class, 'destroy'])->name('destroy');
+    Route::patch('/{customer}/status', [CustomerController::class, 'changeStatus'])->name('change-status');
+    Route::patch('/{customer}/restore', [CustomerController::class, 'restore'])
+        ->withTrashed()
+        ->name('restore');
+});
