92018906ef1a66188b826a542b6c462d77e5e16f
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:15:17 +0330

    feat: add visit API routes

---
+++ routes/api/visit.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\VisitController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('visits')->name('visits.')->group(function () {
+    Route::get('/', [VisitController::class, 'index'])->name('index');
+    Route::post('/', [VisitController::class, 'store'])->name('store');
+    Route::get('/{visit}', [VisitController::class, 'show'])->name('show');
+    Route::put('/{visit}', [VisitController::class, 'update'])->name('update');
+    Route::post('/{visit}/complete', [VisitController::class, 'complete'])->name('complete');
+    Route::post('/{visit}/cancel', [VisitController::class, 'cancel'])->name('cancel');
+});
