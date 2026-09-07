cad07e3273603f28751db8dcd1d593f81d421545
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:09:55 +0330

    feat: add order return API routes

---
+++ routes/api/order-returns.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\OrderReturnController;
+use App\Http\Requests\OrderReturn\AllocateOrderReturnItemRequest;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('order-returns')->name('order-returns.')->group(function () {
+    Route::get('/', [OrderReturnController::class, 'index'])->name('index');
+    Route::post('/', [OrderReturnController::class, 'store'])->name('store');
+    Route::get('/{orderReturn}', [OrderReturnController::class, 'show'])->name('show');
+    Route::put('/{orderReturn}', [OrderReturnController::class, 'update'])->name('update');
+    Route::post('/{orderReturn}/submit', [OrderReturnController::class, 'submit'])->name('submit');
+    Route::post('/{orderReturn}/confirm', [OrderReturnController::class, 'confirm'])->name('confirm');
+    Route::post('/{orderReturn}/complete', [OrderReturnController::class, 'complete'])->name('complete');
+    Route::post('/{orderReturn}/cancel', [OrderReturnController::class, 'cancel'])->name('cancel');
+});
+
+Route::post('/order-return-items/{orderReturnItem}/allocate', [OrderReturnController::class, 'allocate'])
+    ->name('order-return-items.allocate');
