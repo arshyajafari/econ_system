81082b11701511ffe10a66abbccdd6a039786d52
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-08 00:09:26 +0330

    feat: expose order API routes

--- routes/api/order.php
+++ routes/api/order.php
@@ -1,1 +1,15 @@
 <?php
+
+use App\Http\Controllers\Api\OrderController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('orders')->name('orders.')->group(function () {
+    Route::get('/', [OrderController::class, 'index'])->name('index');
+    Route::post('/', [OrderController::class, 'store'])->name('store');
+    Route::get('/{order}', [OrderController::class, 'show'])->name('show');
+    Route::put('/{order}', [OrderController::class, 'update'])->name('update');
+    Route::post('/{order}/submit', [OrderController::class, 'submit'])->name('submit');
+    Route::post('/{order}/confirm', [OrderController::class, 'confirm'])->name('confirm');
+    Route::post('/{order}/complete', [OrderController::class, 'complete'])->name('complete');
+    Route::post('/{order}/cancel', [OrderController::class, 'cancel'])->name('cancel');
+});
