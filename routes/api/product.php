2b3771349626cce2dc5e457671da38f7b457723b
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-16 00:05:13 +0330

    feat: add product image upload endpoint

--- routes/api/product.php
+++ routes/api/product.php
@@ -1,36 +1,14 @@
 <?php
 
-    use App\Http\Controllers\Api\ProductController;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Support\Facades\Route;
 
-    Route::prefix('products')->name('products.')->group(function () {
-        Route::get('/', [
-            ProductController::class,
-            'index'
-        ]);
-
-        Route::post('/', [
-            ProductController::class,
-            'store'
-        ]);
-
-        Route::get('/{product}', [
-            ProductController::class,
-            'show'
-        ]);
-
-        Route::put('/{product}', [
-            ProductController::class,
-            'update'
-        ]);
-
-        Route::delete('/{product}', [
-            ProductController::class,
-            'destroy'
-        ]);
-
-        Route::patch('/{product}/status', [
-            ProductController::class,
-            'changeStatus'
-        ]);
-    });
+Route::prefix('products')->name('products.')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/', [ProductController::class, 'store']);
+    Route::get('/{product}', [ProductController::class, 'show']);
+    Route::post('/{product}/image', [ProductController::class, 'uploadImage']);
+    Route::put('/{product}', [ProductController::class, 'update']);
+    Route::delete('/{product}', [ProductController::class, 'destroy']);
+    Route::patch('/{product}/status', [ProductController::class, 'changeStatus']);
+});
