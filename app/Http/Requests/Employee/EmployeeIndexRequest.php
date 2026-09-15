f1c28f4b6cec243d4cd0e2fe6cfba847b38d30a2
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-15 23:15:59 +0330

    feat: support employee activity filtering

--- app/Http/Requests/Employee/EmployeeIndexRequest.php
+++ app/Http/Requests/Employee/EmployeeIndexRequest.php
@@ -1,29 +1,22 @@
 <?php
 
-    namespace App\Http\Requests\Employee;
+namespace App\Http\Requests\Employee;
 
-    use App\Enums\EmployeeStatus;
-    use App\Enums\EmploymentType;
-    use App\Enums\Gender;
-    use Illuminate\Validation\Rule;
-    use App\Http\Requests\IndexRequest;
+use App\Enums\EmployeeActivityType;
+use App\Enums\EmployeeStatus;
+use App\Enums\EmploymentType;
+use App\Enums\Gender;
+use App\Http\Requests\IndexRequest;
+use Illuminate\Validation\Rule;
 
-    class EmployeeIndexRequest extends IndexRequest {
-        public function rules(): array {
-            return [
-                ...$this->commonRules(),
-                'status' => [
-                    'nullable',
-                    Rule::enum(EmployeeStatus::class),
-                ],
-                'gender' => [
-                    'nullable',
-                    Rule::enum(Gender::class),
-                ],
-                'employment_type' => [
-                    'nullable',
-                    Rule::enum(EmploymentType::class),
-                ],
-            ];
-        }
+class EmployeeIndexRequest extends IndexRequest {
+    public function rules(): array {
+        return [
+            ...$this->commonRules(),
+            'status' => ['nullable', Rule::enum(EmployeeStatus::class)],
+            'gender' => ['nullable', Rule::enum(Gender::class)],
+            'employment_type' => ['nullable', Rule::enum(EmploymentType::class)],
+            'activity_type' => ['nullable', Rule::enum(EmployeeActivityType::class)],
+        ];
     }
+}
