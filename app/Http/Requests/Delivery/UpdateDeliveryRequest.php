eb367e903393c8de4d64e318d26228953db7d061
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-17 19:21:06 +0330

    Make delivery phone and address optional on update

--- app/Http/Requests/Delivery/UpdateDeliveryRequest.php
+++ app/Http/Requests/Delivery/UpdateDeliveryRequest.php
@@ -1,28 +1,30 @@
 <?php
 
-    namespace App\Http\Requests\Delivery;
+namespace App\Http\Requests\Delivery;
 
-    use App\Http\Requests\CrudRequest;
-    use App\Validation\ValidationRules;
+use App\Http\Requests\CrudRequest;
+use App\Validation\ValidationRules;
 
-    class UpdateDeliveryRequest extends CrudRequest {
-        public function rules(): array {
-            return [
-                'recipient_name' => [
-                    'required',
-                    'string',
-                    'max:150',
-                ],
-                'recipient_phone' => [
-                    'required',
-                    'string',
-                    'max:30',
-                ],
-                'address' => [
-                    'required',
-                    'string',
-                ],
-                ...ValidationRules::description(),
-            ];
-        }
+class UpdateDeliveryRequest extends CrudRequest
+{
+    public function rules(): array
+    {
+        return [
+            'recipient_name' => [
+                'required',
+                'string',
+                'max:150',
+            ],
+            'recipient_phone' => [
+                'nullable',
+                'string',
+                'max:30',
+            ],
+            'address' => [
+                'nullable',
+                'string',
+            ],
+            ...ValidationRules::description(),
+        ];
     }
+}
