68455e840eb5973fc5e9adc875dcae12880562c3
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-17 19:20:58 +0330

    Fix customer ledger resource array access

--- app/Http/Resources/CustomerLedgerResource.php
+++ app/Http/Resources/CustomerLedgerResource.php
@@ -1,22 +1,24 @@
 <?php
 
-    namespace App\Http\Resources;
+namespace App\Http\Resources;
 
-    use Illuminate\Http\Request;
-    use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-    class CustomerLedgerResource extends JsonResource {
-        public function toArray(Request $request): array {
-            return [
-                'customer' => [
-                    'id' => $this->customer->public_id,
-                    'name' => $this->customer->customer_name,
-                ],
-                'opening_balance' => $this->opening_balance,
-                'total_debit' => $this->total_debit,
-                'total_credit' => $this->total_credit,
-                'closing_balance' => $this->closing_balance,
-                'transactions' => CustomerLedgerTransactionResource::collection($this->transactions),
-            ];
-        }
+class CustomerLedgerResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'customer' => [
+                'id' => $this->resource['customer']->public_id,
+                'name' => $this->resource['customer']->customer_name,
+            ],
+            'opening_balance' => $this->resource['opening_balance'],
+            'total_debit' => $this->resource['total_debit'],
+            'total_credit' => $this->resource['total_credit'],
+            'closing_balance' => $this->resource['closing_balance'],
+            'transactions' => CustomerLedgerTransactionResource::collection($this->resource['transactions']),
+        ];
     }
+}
