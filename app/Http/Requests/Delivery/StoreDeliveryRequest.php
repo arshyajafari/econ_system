4dfc1d8133187429514cceeca0309f11c69b88e7
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-17 19:21:02 +0330

    Make delivery phone and address optional

--- app/Http/Requests/Delivery/StoreDeliveryRequest.php
+++ app/Http/Requests/Delivery/StoreDeliveryRequest.php
@@ -1,33 +1,35 @@
 <?php
 
-    namespace App\Http\Requests\Delivery;
+namespace App\Http\Requests\Delivery;
 
-    use App\Http\Requests\CrudRequest;
-    use App\Validation\ValidationRules;
+use App\Http\Requests\CrudRequest;
+use App\Validation\ValidationRules;
 
-    class StoreDeliveryRequest extends CrudRequest {
-        public function rules(): array {
-            return [
-                'order_id' => [
-                    'required',
-                    'string',
-                    'exists:orders,public_id',
-                ],
-                'recipient_name' => [
-                    'required',
-                    'string',
-                    'max:150',
-                ],
-                'recipient_phone' => [
-                    'required',
-                    'string',
-                    'max:30',
-                ],
-                'address' => [
-                    'required',
-                    'string',
-                ],
-                ...ValidationRules::description(),
-            ];
-        }
+class StoreDeliveryRequest extends CrudRequest
+{
+    public function rules(): array
+    {
+        return [
+            'order_id' => [
+                'required',
+                'string',
+                'exists:orders,public_id',
+            ],
+            'recipient_name' => [
+                'required',
+                'string',
+                'max:150',
+            ],
+            'recipient_phone' => [
+                'nullable',
+                'string',
+                'max:30',
+            ],
+            'address' => [
+                'nullable',
+                'string',
+            ],
+            ...ValidationRules::description(),
+        ];
     }
+}
