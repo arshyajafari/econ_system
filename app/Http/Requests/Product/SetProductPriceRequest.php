7fda388ae90606f84c2ec134c8031cacb1a8f035
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-17 18:46:35 +0330

    feat: validate product price updates

---
+++ app/Http/Requests/Product/SetProductPriceRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SetProductPriceRequest extends FormRequest
+{
+    public function authorize(): bool { return true; }
+
+    public function rules(): array
+    {
+        return [
+            'sale_price' => ['required', 'numeric', 'min:0'],
+            'effective_from' => ['nullable', 'date'],
+            'description' => ['nullable', 'string'],
+        ];
+    }
+}
