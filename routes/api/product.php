1cb71dfad95b779edd1e32cf74417dc2ee1f710a
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-17 18:46:59 +0330

    feat: add product price route

--- routes/api/product.php
+++ routes/api/product.php
@@ -7,6 +7,7 @@ Route::prefix('products')->name('products.')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
     Route::post('/', [ProductController::class, 'store']);
     Route::get('/{product}', [ProductController::class, 'show']);
+    Route::post('/{product}/price', [ProductController::class, 'setPrice']);
     Route::post('/{product}/image', [ProductController::class, 'uploadImage']);
     Route::put('/{product}', [ProductController::class, 'update']);
     Route::delete('/{product}', [ProductController::class, 'destroy']);
