dcdb238b17467335c882b1024e7ea57b207f812d
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-22 16:21:57 +0330

    fix(scientific-inventory): remove duplicate resource import

--- app/Http/Resources/ScientificVisitorInventoryResource.php
+++ app/Http/Resources/ScientificVisitorInventoryResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources;
 
 use App\Models\ScientificVisitorInventory;
-use App\Models\ScientificVisitorInventory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
