24649ec5b0760fa8125e6c045c704ca4db98f6c3
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-22 16:41:43 +0330

    fix(scientific-inventory): import model traits from App Traits

--- app/Models/ScientificVisitorInventory.php
+++ app/Models/ScientificVisitorInventory.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Traits\HasAudit;
+use App\Traits\HasPublicId;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Traits\HasAudit;
 use App\Traits\HasPublicId;
