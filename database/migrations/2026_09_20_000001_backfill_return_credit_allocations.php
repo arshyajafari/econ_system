d1b94a7cb19830574c0e0e23cb56fd6e3bd27e76
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-22 18:04:00 +0330

    fix: remove ambiguous chunkById from return credit backfill

--- database/migrations/2026_09_20_000001_backfill_return_credit_allocations.php
+++ database/migrations/2026_09_20_000001_backfill_return_credit_allocations.php
@@ -25,7 +25,7 @@ return new class extends Migration {
                     'i.code as invoice_code',
                     'orr.code as return_code',
                 ])
-                ->chunkById(100, function ($transactions) {
+                ->chunk(100, function ($transactions) {
                     foreach ($transactions as $transaction) {
                         $alreadyAllocated = (float) DB::table('customer_credit_allocations')
                             ->where('source_transaction_id', $transaction->source_transaction_id)
