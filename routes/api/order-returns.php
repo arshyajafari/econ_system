aa1f0745004b16d077e35df8de84618cf9029a50
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-22 21:51:37 +0330

    Fix return receive route ordering

--- routes/api/order-returns.php
+++ routes/api/order-returns.php
@@ -7,13 +7,13 @@ Route::prefix('order-returns')->name('order-returns.')->group(function () {
     Route::get('/returnable-orders', [OrderReturnController::class, 'returnableOrders'])->name('returnable-orders');
     Route::get('/', [OrderReturnController::class, 'index'])->name('index');
     Route::post('/', [OrderReturnController::class, 'store'])->name('store');
+    Route::post('/{orderReturn}/receive', [OrderReturnController::class, 'receive'])->name('receive');
     Route::get('/{orderReturn}', [OrderReturnController::class, 'show'])->name('show');
     Route::put('/{orderReturn}', [OrderReturnController::class, 'update'])->name('update');
     Route::post('/{orderReturn}/submit', [OrderReturnController::class, 'submit'])->name('submit');
     Route::post('/{orderReturn}/confirm', [OrderReturnController::class, 'confirm'])->name('confirm');
     Route::post('/{orderReturn}/complete', [OrderReturnController::class, 'complete'])->name('complete');
     Route::post('/{orderReturn}/cancel', [OrderReturnController::class, 'cancel'])->name('cancel');
-    Route::post('/{orderReturn}/receive', [OrderReturnController::class, 'receive'])->name('receive');
 });
 
 Route::post('/order-return-items/{orderReturnItem}/allocate', [OrderReturnController::class, 'allocate'])->name('order-return-items.allocate');
