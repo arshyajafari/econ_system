8a40e020bfdd3500cfb889e6eaeb11dc7af65686
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-22 15:52:16 +0330

    fix(scientific-inventory): import model traits

--- app/Models/ScientificVisitorInventory.php
+++ app/Models/ScientificVisitorInventory.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Concerns\HasPublicId;
+use App\Models\Concerns\HasAudit;
 
 class ScientificVisitorInventory extends BaseModel
 {
