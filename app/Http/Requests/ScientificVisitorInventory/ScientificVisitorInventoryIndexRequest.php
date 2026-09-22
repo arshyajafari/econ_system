4a0629c72b6698eb7bbc2996083f163ddad99e2b
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-22 15:36:44 +0330

    feat(scientific-visitor): add controlled sample inventory

---
+++ app/Http/Requests/ScientificVisitorInventory/ScientificVisitorInventoryIndexRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\ScientificVisitorInventory;
+
+use App\Http\Requests\IndexRequest;
+
+class ScientificVisitorInventoryIndexRequest extends IndexRequest
+{
+    public function rules(): array
+    {
+        return [
+            ...$this->commonRules(),
+            'employee_id' => ['nullable', 'string', 'exists:employees,public_id'],
+            'product_id' => ['nullable', 'string', 'exists:products,public_id'],
+            'available_only' => ['nullable', 'boolean'],
+        ];
+    }
+}
