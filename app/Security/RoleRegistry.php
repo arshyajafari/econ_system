03ff0aca8d8c1c7f4a74914ad6a508a51308cbb7
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-24 21:15:46 +0330

    Grant settlement operators payment update permission

--- app/Security/RoleRegistry.php
+++ app/Security/RoleRegistry.php
@@ -55,7 +55,7 @@ class RoleRegistry {
                 Permission::CUSTOMER_VIEW->value,
                 Permission::ORDER_RETURN_VIEW->value,
                 Permission::PAYMENT_VIEW->value, Permission::PAYMENT_CREATE->value,
-                Permission::PAYMENT_CANCEL->value,
+                Permission::PAYMENT_UPDATE->value, Permission::PAYMENT_CANCEL->value,
                 Permission::DELIVERY_VIEW->value,
             ],
 
