d9e179c7d59441e0c938a39fdcb736f5e0f2ffff
arshyajafari <89856131+arshyajafari@users.noreply.github.com>
2026-09-24 20:25:20 +0330

    refactor: expose customer payable payment balance

--- routes/api/payments.php
+++ routes/api/payments.php
@@ -1,46 +1,14 @@
 <?php
 
-    use App\Http\Controllers\Api\PaymentController;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PaymentController;
+use Illuminate\Support\Facades\Route;
 
-
-    Route::prefix('payments')->name('payments.')->group(function () {
-        Route::get('/', [
-            PaymentController::class,
-            'index',
-        ]);
-
-        Route::post('/', [
-            PaymentController::class,
-            'store',
-        ]);
-
-        Route::get('/{payment}', [
-            PaymentController::class,
-            'show',
-        ]);
-
-        Route::put('/{payment}', [
-            PaymentController::class,
-            'update',
-        ]);
-
-        Route::post('/{payment}/confirm', [
-            PaymentController::class,
-            'confirm',
-        ]);
-
-        Route::post('/{payment}/cancel', [
-            PaymentController::class,
-            'cancel',
-        ]);
-    });
-
-
-
-
-
-
-
-
-
+Route::prefix('payments')->name('payments.')->group(function () {
+    Route::get('/', [PaymentController::class, 'index']);
+    Route::post('/', [PaymentController::class, 'store']);
+    Route::get('/customer/{customer}/payable-balance', [PaymentController::class, 'customerPayableBalance']);
+    Route::get('/{payment}', [PaymentController::class, 'show']);
+    Route::put('/{payment}', [PaymentController::class, 'update']);
+    Route::post('/{payment}/confirm', [PaymentController::class, 'confirm']);
+    Route::post('/{payment}/cancel', [PaymentController::class, 'cancel']);
+});
